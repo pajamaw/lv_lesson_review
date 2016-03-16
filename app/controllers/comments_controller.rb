99bcd91d46441004cfd0b99047e8a6212452c9ce
pajamaw <pjpeterww@gmail.com>
2016-03-16 03:59:40 -0400

    Fixed comment-user-id bug.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @user = User.find_by(id: current_user)
     @lesson = Lesson.find_by(id: params[:lesson_id])
-    @comment= Comment.create(user_id: current_user, lesson_id: params[:lesson_id], content: params[:content])
+    @comment= Comment.create(user_id: @user.id, lesson_id: params[:lesson_id], content: params[:content])
     flash[:notice] = "Comment Added"
     redirect_to lesson_path(@lesson)
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :lesson
-  belongs_to :user #:commenter, class_name: 'User'
+  belongs_to :user #:commenter class_name: 'User'
   has_many :ratings, as: :ratable
 
 
