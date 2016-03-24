d312028b288c9f60067a387cc8f0bfa3de9ce561
pajamaw <pjpeterww@gmail.com>
2016-03-24 11:10:20 -0400

    Added #average_rating to Category class and view

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,12 @@ class Category < ActiveRecord::Base
   has_many :comments, through: :lessons
   has_many :ratings, through: :lessons
  
+  def average_rating
+    average = 0
+    total = 0
+    self.ratings.each do |num|
+      total += num.star_rating.to_f
+    end
+    average = total / self.ratings.count
+  end
 end
